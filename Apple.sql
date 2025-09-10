5f0dfc2c3b6be300ae044422b553d18dc86f1ffb
Jiung Moon <jumoon@ucsd.edu>
2025-09-10 15:03:55 -0700

    Update Apple.sql

--- Apple.sql
+++ Apple.sql
@@ -33,196 +33,221 @@ where store_id = 'ST-31'
 create index sales_store_id on sales(store_id);
 
 -- Business Problems
---1. Find each country and number of stores
-select country, count(*) 
-from  stores
-group by 1
-order by 2 desc;
---2. What is the total number of units sold by each store?
-select s.store_id, st.store_name, sum(quantity) as total_unit_sold
-from sales as s inner join stores as st on s.store_id = st.store_id
-group by 1, 2
-order by 3 DESC;
+/* 	.	Regional Variability
+	•	For each country / city / store_id, compute Claim Probability.
+	•	Highlight outliers vs. national/global average.
+→ Why: Pinpoints geographic hotspots and operational weaknesses. */
 
---3. How many sales occurred in December 2023?
-select count(*) as sales_Dec_2023
-from sales
-where to_char(sale_date, 'MM-YYYY') = '12-2023';
---4. How many stores have never had a warranty claim filed against any of their products?
-
-select * from stores
-where store_id not in(
-				select distinct store_id
-				from sales as s
-				right join warranty as w
-				on s.sale_id = w.sale_id);
-
---5. What percentage of warranty claims are marked as "Warranty Void"?
-select round ((count(*) / (select count(*) from warranty)::numeric * 100), 2) as warranty_void_rate
-from warranty
-where repair_status = 'Warranty Void';
---6. Which store had the highest total units sold in the 2 years?
-select s.store_id, st.store_name, sum(s.quantity)
-from sales as s join stores as st on s.store_id = st.store_id
-where s.sale_date >= current_date - interval '2 year'
-group by 1, 2
-order by 3 desc
-limit 1;
-
---7. Count the number of unique products sold in the last year.
-select p.product_id, p.product_name, sum(s.quantity)
-from products as p join sales as s on p.product_id = s.product_id
-where s.sale_date  >= current_date - interval '2 years'
-group by 1, 2
-order by 3 desc;
---8. What is the average price of products in each category?
-select c.category_name, round(avg(p.price)::numeric, 2) as AVG_price
-from category as c join products as p on c.category_id = p.category_id
-group by 1;
---9. How many warranty claims were filed in 2020?
-select count(*)
-from warranty as w
-where extract(year from w.claim_date ) = 2020;
---10. Identify each store and best selling day based on highest qty sold
-select store_id, day_name, total_unit_sold, rank
-from
-	(select
-	store_id, to_char(sale_date, 'Day') as day_name,
-	sum(quantity) as total_unit_sold,
-	rank() over(partition by store_id order by sum(quantity) DESC) as rank
-	from sales
-	group by 1, 2) as t1
-where rank = 1;
-
---11. Identify least selling product of each country for each year based on total unit sold
-select *	
-from (select st.country, p.product_name, extract(year from s.sale_date), sum(s.quantity) as total_qty_sold,
-	rank() over(partition by st.country, extract(year from s.sale_date) order by sum(s.quantity) asc) as rank
-	from sales as s join stores as st on s.store_id = st.store_id 
-	join products as p on s.product_id = p.product_id
-	group by 1,2,3
-	order by 1,3,4) as product_rank_by_year
-where rank = 1;
-	
---12. How many warranty claims were filed within 180 days of a product sale?
-select count(*)
-from warranty as w
-left join sales as s
-on s.sale_id = w.sale_id
-where w.claim_date - sale_date <= 180;
---13. How many warranty claims have been filed for products launched in the last two years?
-select p.product_name, count(w.claim_id), count(s.sale_id)
-from products as p right join sales as s on p.product_id = s.product_id 
+with high_claim_probability_country
+as (
+select st.country, st.store_name, count(w.claim_id) as num_claim, count(s.sale_id) as num_sales
+from sales as s left join stores as st on s.store_id =st.store_id
 left join warranty as w on s.sale_id = w.sale_id
-where p.launch_date >= current_date - interval '2 years'
-group by 1
-having count(w.claim_id) > 0;
---14. List the months in the last 3 years where sales exceeded 5000 units from usa.
-select to_char(sale_date, 'Month')
-from sales as s join stores as st on s.store_id = st.store_id
-where country = 'USA' and s.sale_date > current_date - interval '3 years'
-group by 1
-having sum(quantity) > 5000;
---15. Which product category had the most warranty claims filed in the last 2 years?
-select c.category_id, p.product_name, count(w.claim_id)
-from category as c join products as p on c.category_id = p.category_id
-join sales as s on p.product_id  = s.product_id 
-join warranty as w on s.sale_id  = w.sale_id
-where w.claim_date > current_date - interval '2 years'
-group by 1, 2
-order by 3 desc
-limit 1;
---16. Determine the percentage chance of receiving claims after each items purchase for each country.
-with t1 as
-(select st.country, p.product_name, round(count(w.claim_id )::numeric / sum(s.quantity )::numeric, 4) * 100 as the_percentage_receiving_claims
-from sales as s left join stores as st on s.store_id = st.store_id 
-left join warranty as w on s.sale_id = w.sale_id
-left join products as p on s.product_id  = p.product_id
-group by 1,2)
-select country, product_name, the_percentage_receiving_claims
-from t1
-where the_percentage_receiving_claims != 0;
---17. Analyze each stores year by year growth ratio
-with yearly_sales
-as(
-select s.store_id, st.store_name, extract(year from sale_date) as year, sum(s.quantity * p.price) as total_sale
-from sales as s join products as p on s.product_id = p.product_id
-join stores as st on s.store_id  = st.store_id
-group by 1, 2, 3
-order by 2,3),
-growth_ratio
-as(
-select store_name, year, LAG(total_sale, 1) over(partition by store_name order by year) as last_year_sale, total_sale as current_year_sale
-from yearly_sales)
-select store_name, year, last_year_sale, current_year_sale, round((current_year_sale::numeric - last_year_sale::numeric)/last_year_sale::numeric * 100, 2)  as growth_ratio_year_by_year
-from growth_ratio
-where last_year_sale is not null and year <> extract(year from current_date);
-/*18. What is the correlation between product price and warranty claims for products sold in the
-last five years? (Segment based on diff price)*/
-select 
-case
-	when p.price < 500 then 'less expensive product'
-	when p.price between 500 and 1000 then 'Mid range product'
-	else 'expensive product'
-end as price_segment,
-count(w.claim_id) as total_claim
-from warranty as w
-join sales as s on w.sale_id = s.sale_id join products as p on p.product_id = s.product_id
-where claim_date >= current_date - interval '5 year'
-group by 1;
-/*19. Identify the store with the highest percentage of "Paid Repaired" claims in relation to total
-claims filed.*/
-with paid_repair
-as 
-(select
-	s.store_id, count(repair_status) as paid_repaired
-from sales as s join warranty as w on s.sale_id = w.sale_id
-where repair_status = 'Paid Repaired'
-group by 1),
-total_repair as(
-select
-	s.store_id, count(repair_status) as total_repaired
-from sales as s join warranty as w on s.sale_id = w.sale_id
-group by 1)
-select
-	p.store_id, paid_repaired, total_repaired, (paid_repaired::numeric / total_repaired::numeric) * 100 as percentage_paid_repaired
-from paid_repair as p join total_repair as t on p.store_id = t.store_id
-order by 3 asc
-limit 1;
-
-/*20.Write SQL query to calculate the monthly running total of sales for each store over the past
-years and compare the trends across this period?*/
-with monthly_sale 
-as
-(select 
-	store_id,
-	extract(year from sale_date) as year,
-	extract(month from sale_date) as month,
-	sum(p.price * s.quantity) as total_revenue
-from sales as s join products as p on s. product_id = p.product_id
-group by 1,2,3
-order by 1, 3)
-select
-	store_id,
-	month,
-	year,
-	total_revenue,
-	sum(total_revenue) over(partition by store_id order by year, month) as running_total
-from monthly_sale;
-
-/*21.Analyze sales trends of product over time, segmented into key time periods: from launch to 6
-months, 6-12 months, 12-18 months, and beyond 18 months?*/
-
-select
-	p.product_name,
-	case
-		when s.sale_date between p.launch_date and p.launch_date + interval '6 month' then '0-6'
-		when s.sale_date between p.launch_date + interval '6 month' and p.launch_date + interval '12 month' then '6-12'
-		when s.sale_date between p.launch_date + interval '12 month' and p.launch_date + interval '18 month' then '12-18'
-		else '18+'
-	end as plc,
-	sum(s.quantity) as total_qty_sale
-from sales as s join products as p on s.product_id = p.product_id
 group by 1,2
-order by 1,2 asc;
-	
+order by 3 desc
+limit 15
+)
+select country, store_name, num_claim, num_sales, round(num_claim::numeric /num_sales::numeric * 100, 2) as claim_probability_by_stores
+from high_claim_probability_country
+order by 3 desc;
+
+/*
+Country       Store Name                Claims   Sales    Claim Probability (%)
+-------------------------------------------------------------------------------
+Spain         Apple Barcelona            6052     9321           64.93
+UAE           Apple Mall of the Emirates 5978     6026           99.20
+UAE           Apple Dubai Mall           5838    10805           54.03
+Turkey        Apple Ankara               3126    26672           11.72
+Turkey        Apple Istanbul             3031    26512           11.43
+India         Apple New Delhi            2241    26650            8.41
+Italy         Apple Milan                1691    12738           13.28
+UK            Apple Regent Street        1214    21075            5.76
+Germany       Apple Munich                481    21015            2.29
+UK            Apple Covent Garden         267    21117            1.26
+UK            Apple Bluewater             259    20993            1.23
+France        Apple Lyon                  184    12837            1.43
+Netherlands   Apple Amsterdam             163    12874            1.27
+Germany       Apple Kurfürstendamm        160    12717            1.26
+France        Apple Champs-Élysées        151    12644            1.19
+*/
+
+
+ /*  Lifecycle Risk
+	•	For each product, bucket claims by time since launch:
+	•	0–6 months (early failure)
+	•	7–12 months
+	•	13+ months (mature)
+	•	Compare Claim Probability across buckets.*/
+
+with claim
+as(
+select p.product_name, p.launch_date, w.claim_date , w.claim_id
+from products as p right join sales as s on p.product_id  = s.product_id
+left join warranty as w on s.sale_id = w.sale_id
+where w.claim_date is not null
+group by 1,2,3,4)
+select product_name, count(claim_id),
+case
+	when claim_date::date - launch_date::date between 0 and 180 then '0-6 months'
+	when claim_date::date - launch_date::date between 181 and 360 then '7-12 months'
+	else '13+ months(mature)'
+end
+, rank() over(partition by product_name order by count(claim_id) desc) as rank
+from claim
+group by 1,3
+order by 1;
+
+/* Product                           Claims   Lifecycle        Rank
+----------------------------------------------------------------
+### 🎧 Audio Devices
+AirPods (3rd Gen)                    64    0-6 months        1
+AirPods (3rd Gen)                    35    7-12 months       2
+AirPods (3rd Gen)                    19    13+ months        3
+AirPods Max                          18    0-6 months        1
+AirPods Max                           9    7-12 months       2
+AirPods Max                           5    13+ months        3
+AirPods Pro (2nd Gen)               299    0-6 months        2
+AirPods Pro (2nd Gen)               384    7-12 months       1
+AirPods Pro (2nd Gen)               230    13+ months        3
+AirTag                              876    0-6 months        3
+AirTag                             2332    7-12 months       1
+AirTag                             1889    13+ months        2
+HomePod mini                         40    0-6 months        1
+HomePod mini                         26    7-12 months       2
+HomePod mini                          8    13+ months        3
+
+### 📺 Streaming / Subscription
+Apple Fitness+                       16    0-6 months        1
+Apple Fitness+                       15    7-12 months       2
+Apple Fitness+                        5    13+ months        3
+Apple One                            151   0-6 months        3
+Apple One                            164   7-12 months       2
+Apple One                           1065   13+ months        1
+Apple TV 4K                           59   0-6 months        3
+Apple TV 4K                          160   7-12 months       1
+Apple TV 4K                          112   13+ months        2
+
+### ⌚ Wearables
+Apple Watch Series 5                  35   7-12 months       2
+Apple Watch Series 5                1015   13+ months        1
+Apple Watch Series 4                 824   13+ months        1
+Apple Watch Series 7                  59   0-6 months        1
+Apple Watch Series 7                  32   7-12 months       2
+Apple Watch Series 7                  25   13+ months        3
+
+### 💻 Macs
+Mac mini (M1)                         34   0-6 months        1
+Mac mini (M1)                         16   7-12 months       3
+Mac mini (M1)                         17   13+ months        2
+MacBook Air (M1)                      58   0-6 months        1
+MacBook Air (M1)                      21   7-12 months       2
+MacBook Air (M1)                       9   13+ months        3
+MacBook Pro (M1 Max, 16-inch)        343   0-6 months        1
+MacBook Pro (M1 Max, 16-inch)        292   7-12 months       2
+MacBook Pro (M1 Max, 16-inch)        140   13+ months        3
+MacBook Pro (M1 Pro, 14-inch)        327   0-6 months        1
+MacBook Pro (M1 Pro, 14-inch)        286   7-12 months       2
+MacBook Pro (M1 Pro, 14-inch)        151   13+ months        3
+MacBook Pro (M1, 13-inch)             36   0-6 months        1
+MacBook Pro (M1, 13-inch)             19   7-12 months       2
+MacBook Pro (M1, 13-inch)             12   13+ months        3
+iMac (24-inch, M1)                    60   0-6 months        3
+iMac (24-inch, M1)                   134   7-12 months       1
+iMac (24-inch, M1)                   120   13+ months        2
+
+### 📱 iPad
+iPad (10th Gen)                      224   0-6 months        2
+iPad (10th Gen)                      260   7-12 months       1
+iPad (10th Gen)                      201   13+ months        3
+iPad (7th Gen)                        39   7-12 months       2
+iPad (7th Gen)                       983   13+ months        1
+iPad (6th Gen)                       781   13+ months        1
+iPad (9th Gen)                        37   0-6 months        2
+iPad (9th Gen)                        46   7-12 months       1
+iPad (9th Gen)                        27   13+ months        3
+iPad Air (4th Gen)                   109   0-6 months        3
+iPad Air (4th Gen)                   189   7-12 months       2
+iPad Air (4th Gen)                  1076   13+ months        1
+iPad Pro (M1, 11-inch)                61   0-6 months        3
+iPad Pro (M1, 11-inch)               135   7-12 months       1
+iPad Pro (M1, 11-inch)               126   13+ months        2
+iPad Pro (M1, 12.9-inch)              55   0-6 months        3
+iPad Pro (M1, 12.9-inch)             153   7-12 months       1
+iPad Pro (M1, 12.9-inch)             127   13+ months        2
+iPad Pro (M2, 11-inch)               246   0-6 months        2
+iPad Pro (M2, 11-inch)               297   7-12 months       1
+iPad Pro (M2, 11-inch)               173   13+ months        3
+
+### 📱 iPhone
+iPhone 12                            67   0-6 months        1
+iPhone 12                            58   7-12 months       2
+iPhone 12                            29   13+ months        3
+iPhone 12 Mini                       48   0-6 months        1
+iPhone 12 Mini                       24   7-12 months       2
+iPhone 12 Mini                       11   13+ months        3
+iPhone 12 Pro                        88   0-6 months        1
+iPhone 12 Pro                        46   7-12 months       2
+iPhone 12 Pro                        19   13+ months        3
+iPhone 12 Pro Max                    45   0-6 months        1
+iPhone 12 Pro Max                    22   7-12 months       2
+iPhone 12 Pro Max                    11   13+ months        3
+iPhone 13                            48   0-6 months        2
+iPhone 13                            55   7-12 months       1
+iPhone 13                            15   13+ months        3
+iPhone 13 Mini                     1156   13+ months        1
+iPhone 13 Pro                        54   0-6 months        2
+iPhone 13 Pro                        64   7-12 months       1
+iPhone 13 Pro                        33   13+ months        3
+iPhone 13 Pro Max                    48   0-6 months        1
+iPhone 13 Pro Max                    36   7-12 months       2
+iPhone 13 Pro Max                    24   13+ months        3
+iPhone 14                           454   0-6 months        2
+iPhone 14                           539   7-12 months       1
+iPhone 14                           328   13+ months        3
+iPhone 14 Pro                       405   0-6 months        2
+iPhone 14 Pro                       534   7-12 months       1
+iPhone 14 Pro                       321   13+ months        3
+iPhone 15                           193   0-6 months        1
+iPhone 15                           128   7-12 months       2
+iPhone 15 Pro                       177   0-6 months        1
+iPhone 15 Pro                       113   7-12 months       2
+iPhone 15 Pro Max                   189   0-6 months        1
+iPhone 15 Pro Max                   131   7-12 months       2
+iPhone 11                            49   7-12 months       2
+iPhone 11                           946   13+ months        1
+iPhone 11 Pro                        51   7-12 months       2
+iPhone 11 Pro                       989   13+ months        1
+iPhone 11 Pro Max                    35   7-12 months       2
+iPhone 11 Pro Max                   907   13+ months        1
+iPhone SE (3rd Gen)                  28   7-12 months       2
+iPhone SE (3rd Gen)                1857   13+ months        1
+iPhone X                            798   13+ months        1
+iPhone XR                           766   13+ months        1
+iPhone XS                           746   13+ months        1
+iPhone XS Max                       760   13+ months        1
+
+*/
+
+/*   Product / Category Hotspots
+	•	For each category_name and product_name, compute Claim Probability.
+	•	Rank by risk (with a minimum sales threshold).
+→ Why: Identifies a few problematic products driving disproportionate costs. */
+
+select category_name, product_name, count(claim_id), count(s.sale_id), round((count(claim_id)::numeric / count(s.sale_id)::numeric * 100), 2) as claim_probability_by_product
+, rank() over(order by round((count(claim_id)::numeric / count(s.sale_id)::numeric * 100), 2) desc)
+from category as c right join products as p on c.category_id  = p.category_id 
+right join sales as s on p.product_id  = s.product_id 
+left join warranty as w on s.sale_id = w.sale_id 
+group by 1, 2
+order by 5 desc;
+-- Claim Probability
+/* 	1.	iPad Air (4th Gen) — 24.85%
+	2.	Apple One (Subscription Service) — 24.71%
+	3.	Apple Watch Series 5 — 20.27%
+	4.	iPhone 11 Pro — 20.23%
+	5.	iPad (7th Gen) — 19.59%
+	6.	iPhone 11 — 19.52%
+	7.	iPhone 11 Pro Max — 18.53%
+	8.	iPhone 13 Mini — 18.12%
+	9.	iPhone SE (3rd Gen) — 13.82%
+	10.	Apple Watch Series 4 — 4.14% */
